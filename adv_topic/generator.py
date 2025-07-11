dd36ee069a75bfff84e73abaa37fb9ad32519b7d
GowdhamanBJ-7 <gowdhaman.bj@diggibyte.com>
2025-07-11 10:49:57 +0530

    advance topic

--- adv_topic/generator.py
+++ adv_topic/generator.py
@@ -1,5 +1,24 @@
-def demo():
-    yield 5
+def simple_generator():
+    yield "Hello"
+    yield "World"
+    yield "!"
+
+# Using the generator
+for value in simple_generator():
+    print(value)
 
 
-print(demo())
+def count_up_to(n):
+    count = 1
+    while count <= n:
+        yield count
+        count += 1
+gen = count_up_to(4)
+print(next(gen))  # 1
+print(next(gen))  # 2
+print(next(gen))  # 3
+# print(next(gen))  # Raises StopIteration
+
+#using loop
+for i in gen:
+    print(i)
---
+++ adv_topic/highorder.py
@@ -0,0 +1,26 @@
+# A lambda function to add two numbers
+add = lambda x, y: x + y
+print(add(4,8))
+
+num=[1,2,3,4,5]
+square=map(lambda x: x**2 , num)
+print (square)
+#Output: <map object at 0x000001C92726BAC0>
+print (list(square))
+#Output: [1, 4, 9, 16, 25]
+
+def cube(x):
+    return x**3
+cube = map(cube, num)
+print(cube)
+print(list(cube))
+
+#filter
+number = [1,2,3,4,5,67]
+even = filter(lambda num1: num1 % 2==0, number)
+print(list(even))
+
+programming = ['python','java','c++','c']
+programming_len = filter(lambda x: len(x)>5,programming)
+print(list(programming_len)) #output ['python']
+
